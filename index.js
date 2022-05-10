9a351c03a7c15ba21d625f0fc4f828a354ec5faf
gongyoon93 <qkszn93@gmail.com>
2022-05-10 20:43:00 +0900

    mongoDB model&schema SSH BodyParser Nodemon division(private setting)

--- index.js
+++ index.js
@@ -2,16 +2,44 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+const bodyParser = require('body-parser');
+
+const config = require('./config/key');
+
+const { User } = require('./models/Users');
+
+//client에서 보낸 application/x-www-form-urlencoded를 분석해서 가져옴
+app.use(bodyParser.urlencoded({extend: true}));
+
+//application/json
+app.use(bodyParser.json()); 
+
 //mongoDB와 app을 연결
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://gongyoon:abcd1234@cluster0.emgti.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
+mongoose.connect(config.mongoURI,{
   useNewUrlParser: true, useUnifiedTopology: true
 }).then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err))
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hi everyone')
 })
+
+app.post('/register', (req, res) => {
+
+  // 회원가입시 필요한 정보들을 client에 서 가져오면
+  // DB에 저장
+
+    const user = new User(req.body);
+
+    user.save((err, userInfo) => {
+      if(err) return res.json({ successs: false, err});
+      //request 요청 수행 완료(200)
+      return res.status(200).json({
+        success: true
+      })
+    });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
