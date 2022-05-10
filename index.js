c496c25948810a84090400ea1b132c23db83a310
gongyoon93 <qkszn93@gmail.com>
2022-05-10 15:29:51 +0900

    second commit

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+//mongoDB와 app을 연결
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://gongyoon:abcd1234@cluster0.emgti.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
+  useNewUrlParser: true, useUnifiedTopology: true
+}).then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err))
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
@@ -9,3 +16,4 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
