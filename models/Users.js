d7b244bf9a130dfc5f40aa845e525aff16040c0b
gongyoon93 <qkszn93@gmail.com>
2022-05-11 20:33:48 +0900

    bcrypt password

--- models/Users.js
+++ models/Users.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
 const userSchema = mongoose.Schema({
     name: {
         type: String,
@@ -6,7 +9,11 @@ const userSchema = mongoose.Schema({
     },
     email: {
         type: String,
-        trim: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        minLength: 5
     },
     lastname: {
         type: String,
@@ -23,8 +30,32 @@ const userSchema = mongoose.Schema({
     tokenExp: {
         type: Number
     }
-});  
+});
+
+userSchema.pre('save', function(next){
+    let user = this;
+
+    if (user.isModified('password')) {
+        //비밀번호를 암호화 시킨다.
+        bcrypt.genSalt(saltRounds, function(err, salt){
+            if (err) 
+                return next(err);
+            
+            bcrypt.hash(user.password, salt, function(err, hash){
+                if (err) 
+                    return next(err);
+                user.password = hash;
+                next();
+            })
+        })
+    } else {
+        next();
+    }
+
+});
 
 const User = mongoose.model('User', userSchema);
 
-module.exports = {User};
+module.exports = {
+    User
+};
