7a57321cf6fcfbe1a2e34d884795022a92ff40b0
gongyoon93 <qkszn93@gmail.com>
2022-05-16 16:03:10 +0900

    redux up

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,26 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import 'antd/dist/antd.css';
+import { Provider } from 'react-redux';
+import { applyMiddleware, createStore } from 'redux';
+import promiseMiddleware from 'redux-promise';
+import ReduxThunk from 'redux-thunk';
+import Reducer from './_reducers';
+
+//promise, function 형태도 action으로 받을수 있게 지원해줌
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider
+        store={createStoreWithMiddleware(Reducer,
+          window.__REDUX_DEVTOOLS_EXTENSION__ &&
+          window.__REDUX_DEVTOOLS_EXTENSION__()
+          )}
+    >
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
