82c38c8193ba0d57d976fdf7af413da6fa5bf5f3
gongyoon93 <qkszn93@gmail.com>
2022-05-17 14:08:04 +0900

    logout

--- client/src/components/views/LandingPage/Landingpage.js
+++ client/src/components/views/LandingPage/Landingpage.js
@@ -1,16 +1,36 @@
 import React, { useEffect } from 'react'
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 function Landingpage() {
+  const navigate = useNavigate();
     // useEffect(() => {
     //     axios.get('/api/hello')
     //     .then(res => console.log(res.data));
     // },[]);
+
+  const onClickHandler = () => {
+      axios.get('api/users/logout')
+      .then(res => {
+        if(res.data.success){
+          alert('로그아웃 되었습니다.');
+          navigate('/login');
+        }else{
+          alert('로그아웃에 실패하였습니다.');
+        }
+      });
+  };
+
   return (
     <div style={{
-      display: 'flex', justifyContent: 'center', alignItems: 'center'
+      display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'
       , width: '100%', height: '100vh'
-    }}>시작 페이지</div>
+    }}>
+      <h2>시작 페이지</h2>
+  
+      <button onClick={onClickHandler}>
+        로그아웃</button>
+    </div>
   )
 }
 
